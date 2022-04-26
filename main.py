6fa7529966004bb4f9342da0893d1b863de2fb96
Athirson <Athirsonarceus@gmail.com>
2022-04-25 21:21:26 -0300

    Nothing special

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-#Note! For the code to work you need to replace all the placeholders with
-#Your own details. e.g. account_sid, lat/lon, from/to phone numbers.
+# Note! For the code to work you need to replace all the placeholders with
+# Your own details. e.g. account_sid, lat/lon, from/to phone numbers.
 
-import requests
 import os
-from twilio.rest import Client
+from os.path import dirname
+from os.path import join
+import requests
+from dotenv import find_dotenv
+from dotenv import load_dotenv
 from twilio.http.http_client import TwilioHttpClient
+from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+LAT = -23.534640
+LNG = -46.423770
+load_dotenv(find_dotenv())
 api_key = os.environ.get("OWM_API_KEY")
-account_sid = "YOUR ACCOUNT SID"
-auth_token = os.environ.get("AUTH_TOKEN")
+account_sid = "AC3e11ff847021fcaea9eb2632457c9e71"
+auth_token = os.environ.get("2c001171121034fffd1e08d88e3a09a2")
 
 weather_params = {
-    "lat": "YOUR LATITUDE",
-    "lon": "YOUR LONGITUDE",
+    "lat": LAT,
+    "lon": LNG,
     "appid": api_key,
     "exclude": "current,minutely,daily"
 }
@@ -37,8 +44,8 @@ if will_rain:
     client = Client(account_sid, auth_token, http_client=proxy_client)
     message = client.messages \
         .create(
-        body="It's going to rain today. Remember to bring an ☔️",
-        from_="YOUR TWILIO VIRTUAL NUMBER",
-        to="YOUR TWILIO VERIFIED REAL NUMBER"
-    )
+            body="It's going to rain today. Remember to bring an ☔️",
+            from_="YOUR TWILIO VIRTUAL NUMBER",
+            to="YOUR TWILIO VERIFIED REAL NUMBER"
+        )
     print(message.status)
